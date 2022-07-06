159cd83e877f9cc72cceab8f792a20a22822d38b
n0120 <free_dial@hotmail.co.jp>
2022-07-06 12:05:07 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,123 @@
+const PrefectureCheckbox = {
+  data() {
+    return {
+      prefectures: [
+        '北海道',
+        '青森県',
+        '岩手県',
+        '宮城県',
+        '秋田県',
+        '山形県',
+        '福島県',
+        '茨城県',
+        '栃木県',
+        '群馬県',
+        '埼玉県',
+        '千葉県',
+        '東京都',
+        '神奈川県',
+        '新潟県',
+        '富山県',
+        '石川県',
+        '福井県',
+        '山梨県',
+        '長野県',
+        '岐阜県',
+        '静岡県',
+        '愛知県',
+        '三重県',
+        '滋賀県',
+        '京都府',
+        '大阪府',
+        '兵庫県',
+        '奈良県',
+        '和歌山県',
+        '鳥取県',
+        '島根県',
+        '岡山県',
+        '広島県',
+        '山口県',
+        '徳島県',
+        '香川県',
+        '愛媛県',
+        '高知県',
+        '福岡県',
+        '佐賀県',
+        '長崎県',
+        '熊本県',
+        '大分県',
+        '宮崎県',
+        '鹿児島県',
+        '沖縄県',
+      ],
+    };
+  },
+  /* html */
+  template: `
+  <span v-for="(prefecture, index) in prefectures">
+    <input type="checkbox" v-bind:id="'e' + index">
+    <label v-bind:for="'e' + index">{{ prefecture }}</label>
+  </span>
+  `,
+};
+
+const PopulationBarPlot = {
+  props: [ 'api' ],
+  data() {
+    return {
+      populations: [ 100, 90, 80, 70, 60, 50, 40, 30, 20, 10 ],
+      result: '',
+    };
+  },
+  /* html */
+  template: `
+  <div>{{ result }}</div>
+  <button v-on:click="updateGraph">更新</button>
+  <div class="container">
+    <div
+      v-for="population in populations"
+      class="item"
+      v-bind:style="'height: ' + population + 'px;'"
+    ></div>
+  </div>
+  `,
+  methods: {
+    async updateGraph() {
+
+      let data = await getPopulations(this.api, 27);
+      // dataを加工
+      obj = data["result"]["data"][0]["data"];
+      // forを使ってvalueのみを取り出す
+      // let values = [];
+      // for (let i = 0; i < obj.length; i++) {
+      //   values.push(obj[i]["value"]);
+      // }
+      // mapを使ってvalueのみを取り出す
+      let values = obj.map(function(item) {
+        return item["value"];
+      }
+      );
+      // this.result = values;
+      this.populations = values;
+      // グラフを縮尺して表示
+      this.populations = this.populations.map(function(item) {
+        return item / 15000;
+      }
+      );
+    },
+  },
+};
+
+const RootComponent = {
+  data() {
+    return {
+      'api': 'gc7Aq5dppJetPXJYHaIDk7IZEWOuCTioYGiICBvg',
+    };
+  },
+  components: {
+    PrefectureCheckbox,
+    PopulationBarPlot,
+  },
+};
+
+Vue.createApp(RootComponent).mount('#app');
